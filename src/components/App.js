e4cdc91248eadd1d669bb3804f81de3b4f318542
mixed-up8 <numa.takashi@nifty.com>
2020-11-23 12:14:37 +0900

    display events, refactor and implement remove event button

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, {useReducer, useState} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Event from './Event'
-import reducer from '../reducers'
+import reducer from '../reducers' // eventsって名前でexportしたけどここではreducerと別の名前でimportした。
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
@@ -9,15 +9,16 @@ const App = () => {
   const [body, setBody] = useState('');
 
   const addEvent = e => {
+    // eventのデフォルトの挙動を防止する＝画面が白くならない。必要な個所だけレンダリングする
     e.preventDefault();
   dispatch({
-    type: 'CREATE_EVENT',
+    type: 'CREATE_EVENT', // カッコのなかはaction。type属性が必要
     title,
     body
   })
   setTitle('')
   setBody('')
-}
+  }
 
   return (
     <div className="container-fluid">
@@ -31,7 +32,8 @@ const App = () => {
 
         <div className="form-group">
           <label htmlFor="formEventBody">ボディ</label>
-          <textarea className="form-control" id="formEventBody"/>
+          <textarea className="form-control" id="formEventBody" value={body} onChange={
+            e => setBody(e.target.value)}/>
         </div>
         <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
         <button className="btn btn-danger">全てのイベントを削除する</button>
@@ -47,7 +49,9 @@ const App = () => {
          </tr>
         </thead>
         <tbody>
-          { state.map(( event, index) => (<Event key={index} event={event} dispatch={dispatch}/>))}
+          { state.map(( event, index) => (
+          <Event key={index} event={event} dispatch={dispatch}/>
+          ))}
         </tbody>
       </table>
     </div>
